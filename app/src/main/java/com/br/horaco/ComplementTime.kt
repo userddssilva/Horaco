c53d9950ed833aed724bdc6b4decb1f9df4545b7
Dayvson Silva <userddssilva@gmail.com>
2020-09-02 19:52:48 -0400

    Add new activity (activity_complement_time.xml)
    Signed-off-by: Dayvson Silva <userddssilva@gmail.com>

---
+++ app/src/main/java/com/br/horaco/ComplementTime.kt
@@ -0,0 +1,11 @@
+package com.br.horaco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ComplementTime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complement_time)
+    }
+}
