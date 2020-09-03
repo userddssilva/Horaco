0cb937541589f6c95251f9f621443cef06d20703
Dayvson Silva <userddssilva@gmail.com>
2020-09-03 13:51:55 -0400

    creating new fragments and try visualize that
    Signed-off-by: Dayvson Silva <userddssilva@gmail.com>

---
+++ app/src/main/java/com/br/horaco/ui/comptime/ComptimeViewModel.kt
@@ -0,0 +1,12 @@
+package com.br.horaco.ui.comptime
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ComptimeViewModel : ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
