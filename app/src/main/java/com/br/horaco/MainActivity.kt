065c04d7d17ab35e6719a1bd84415e39f4802827
Dayvson Silva <ddss.snf19@uea.edu.br>
2020-09-17 23:05:12 -0400

    creating new fragments and try visualize that
    Signed-off-by: Dayvson Silva <ddss.snf19@uea.edu.br>

--- app/src/main/java/com/br/horaco/MainActivity.kt
+++ app/src/main/java/com/br/horaco/MainActivity.kt
@@ -36,7 +36,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.comptimeFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/br/horaco/ui/comptime/ComptimeFragment.kt
+++ app/src/main/java/com/br/horaco/ui/comptime/ComptimeFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.br.horaco.R
 
 class ComptimeFragment : Fragment() {
+
+    private lateinit var comptimeViewModel: ComptimeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comptime, container, false)
+        comptimeViewModel =
+            ViewModelProviders.of(this).get(ComptimeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_comptime, container, false)
+        val textView: TextView = root.findViewById(R.id.textView3)
+        comptimeViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
     }
 }
